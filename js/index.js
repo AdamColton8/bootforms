cee7d3cc6dbb3589979c10f6b88292784e82c6a3
Alexandr <alexsot1545@gmail.com>
2017-11-20 00:27:07 +0200

    Js fix

--- js/index.js
+++ js/index.js
@@ -1,11 +1,46 @@
 $('#add_button').click(function(){
    var row = $('#source_row').html();   
-   console.log(row);
+  
    $('#source_wrap').append(row);
+
    $('#source_wrap .col-md-12:last-child .toggle_hidden').bootstrapToggle({
-    on: 'PARCEL',
-    off: 'PALLET',
-    onstyle: 'primary',
-    offstyle: 'primary'
+      on: 'PARCEL',
+      off: 'PALLET',
+      onstyle: 'primary',
+      offstyle: 'primary'
    });
+
+   $isChecked = $('#source_wrap .col-md-12:last-child input[type="checkbox"]').prop("checked");
+   $('#source_wrap .col-md-12:last-child').addClass("active-_first");
+
+
+
+
+
+
   });
+
+$("body").delegate("input[type='checkbox']" , 'change' , function () {
+    if($(this).prop("checked") === false) {
+
+        $(this).parents('.col-md-12').removeClass("active-_second").addClass("active-_first");
+    } else {
+
+        $(this).parents('.col-md-12').removeClass("active-_first").addClass("active-_second");
+    }
+});
+
+
+
+$("#remove_button").click(function () {
+  if($("#source_wrap .col-md-12").length > 1) {
+        $("#source_wrap .col-md-12:last-child").remove()
+  }
+});
+
+
+$("#form_class").on("submit" ,function (e) {
+   
+
+   return false;
+})
